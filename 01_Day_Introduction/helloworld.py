51adb6369bb73f6c8af3480dbb3d3f43d6ddea5b
twinkeltoes <142402975+twinkeltoes@users.noreply.github.com>
2024-08-18 13:24:05 +0100

    added definitions of type.

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -11,11 +11,18 @@ print(3 // 2)  # Floor division operator(//)
 
 # Checking data types
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(10))                  # Int - positve or negatice whole number
+print(type(3.14))                # Float - positive or negative decimal number
+print(type(1 + 3j))              # Complex - imaginay numbers
+print('Asabeneh said "hi meows "')        # String - collection of characters wrapped in speech marks.
+print(type([1, 2, 3]))           # List     -    collections of values seperated by commas and can be any data type.
+print(type({'name':'Asabeneh'})) # Dictionary     - a collection of key value pair which. keys ahve tp be unique. 
+print({9.8, 3.14, 2.7, "hi" , 'hello' , "hi" })    # Set         -  is a list of  unique data types that . it prints out only unique values and different data types. and unordered
+print((9.8, 3.14, 2.7, "meow moew ", 5j))    # Tuple   - a collection of different data types that are ordered in an immutable list. and allows duplicates.
+
+
+# maths equation
+print(5*5)
+print((10*2)+5)
+
+print(type(2.5+2.5))
